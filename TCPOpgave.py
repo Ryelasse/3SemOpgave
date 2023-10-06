75aec1ef3716931d01cd4db1ec63be1c220adee8
Ryelasse <lass375s@edu.zealand.dk>
2023-10-06 12:02:16 +0200

    first commit

---
+++ TCPOpgave.py
@@ -0,0 +1,55 @@
+from socket import *
+from threading import *
+import random
+
+serverPort = 9
+
+
+
+
+def handleClient(clientSocket, addr):
+    ask = f'choose an option:\n 1, add [nam] [nam]\n 2. substract [nam] [nam]\n 3. random [nam] [nam]\n'
+    clientSocket.send(ask.encode())
+    while True:  
+        
+        sentence = clientSocket.recv(2048).decode()
+        if not sentence:
+            break
+       
+
+        splitText = sentence.split()
+        Text=''
+        if(splitText[0].lower() == 'add'):
+            talx = int (splitText[1])
+            taly = int (splitText[2])
+            Text = f'{talx} + {taly} = {(talx+taly)}\n\n' 
+        
+        elif (splitText[0].lower() == 'random'):
+             talx = int (splitText[1])
+             taly = int (splitText[2])
+             Text = f' {(random.randint(talx,taly))}\n\n'
+       
+        elif (splitText[0].lower() == 'substract'):
+             talx = int (splitText[1])
+             taly = int (splitText[2])
+             Text = f'{talx} - {taly} = {(talx-taly)}\n\n' 
+
+
+        else:
+            Text = f'understøtter ikke metoden {splitText[0]}'
+
+        clientSocket.send(Text.encode())
+    
+
+
+serverSocket = socket (AF_INET,SOCK_STREAM)
+serverSocket.bind(("",serverPort))
+serverSocket.listen(1)
+print('The server is ready to receive', serverSocket)
+
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    print('forbinder til en klient fra adressen', addr)
+    Thread(target=handleClient, args=(connectionSocket, addr)).start()
+    
